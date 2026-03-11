d8f521726d730e644fb3ab5c4eb51b378fc1640b
ibrahimallmamun <mimamun@isrt.ac.bd>
2026-03-11 20:27:56 +0600

    removed migrations

--- backend/prisma/migrations/20260307190847_add_registration_open/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "aspl_seasons" ADD COLUMN     "registration_open" BOOLEAN NOT NULL DEFAULT false;
--- backend/prisma/migrations/20260311133411_added_blood_group/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "member_update_requests" ADD COLUMN     "blood_group" TEXT;
-
--- AlterTable
-ALTER TABLE "members" ADD COLUMN     "blood_group" TEXT;
